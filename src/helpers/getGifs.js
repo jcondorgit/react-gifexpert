ca8e784bf159d0db827e8046bc2b64693956d864
Jonny Condor <45129411+jcondorgit@users.noreply.github.com>
2024-02-13 12:07:58 -0500

    Update getGifs.js

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=I3yiGAiTcTqlw0RqPVlQ1eCJXlewYbDz&q=${category}&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=MiapyKeyValue=${category}&limit=10`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
